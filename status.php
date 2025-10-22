20fa023fecaadb25ff9ffbb0814a378f58fc421f
Reid <88220593+DevDodger@users.noreply.github.com>
2025-10-21 23:01:34 -0700

    Remove server status check from status.php

--- status.php
+++ status.php
@@ -1,13 +1,1 @@
-<?php
-$ip = "devdodger.tech";
-$port = 25565;
 
-$fp = @fsockopen($ip, $port, $errno, $errstr, 2);
-
-if ($fp) {
-    echo "🟢 Server is Online";
-    fclose($fp);
-} else {
-    echo "🔴 Server is Offline";
-}
-?>
